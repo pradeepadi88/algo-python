f8f509bd03d6746efd79435a5f939b3ee2d254e1
Pradeep Adimoolam <pradeepadi88@gmail.com>
2023-02-21 14:24:08 -0700

    Update naive_k_sum.py

--- Array/naive_k_sum.py
+++ Array/naive_k_sum.py
@@ -8,20 +8,19 @@ class Solution:
     def max_sum_of_subarray(array: list, n, k):
         max_sum = 0
         if n < k:
-            for i in range(array):
+            for i in range(n):
                 max_sum += array[i]
         for i in range(n - k + 1):
             sum = array[i]
             for j in range(i+1, i+k):
                 sum += array[j]
             max_sum = max(sum, max_sum)
-            print(f"max_sum {max_sum}")
         return max_sum
 
 
 if __name__ == "__main__":
     arr = [1, 4, 2, 10, 2, 3, 1, 0, 20]
-    k = 4
+    k = 11
     n = len(arr)
     max_sum = Solution.max_sum_of_subarray(arr, n, k);
     print(max_sum);
