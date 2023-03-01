5a8b02f589ad414cce5fb2f29881af7f66af24e9
Pradeep Adimoolam <pradeepadi88@gmail.com>
2023-03-01 10:26:25 -0700

    Update naive_k_sum.py

--- Array/naive_k_sum.py
+++ Array/naive_k_sum.py
@@ -12,15 +12,34 @@ class Solution:
                 max_sum += array[i]
         for i in range(n - k + 1):
             sum = array[i]
-            for j in range(i+1, i+k):
+            for j in range(i + 1, i + k):
                 sum += array[j]
             max_sum = max(sum, max_sum)
         return max_sum
 
+    @staticmethod
+    def max_sum_of_Subarray_sliding_window_1(array: list, n, k):
+        sum = 0
+        for i in range(k):
+            sum += array[i]
+        max_sum = sum
+        for i in range(k, n):
+            sum += (array[i] - array[i - k])
+            max_sum = max(sum, max_sum)
+        return max_sum
+
+    @staticmethod
+    def max_sum_of_Subarray_sliding_window_2(array: list, n, k):
+
+
+
+
 
 if __name__ == "__main__":
     arr = [1, 4, 2, 10, 2, 3, 1, 0, 20]
-    k = 11
+    k = 4
     n = len(arr)
-    max_sum = Solution.max_sum_of_subarray(arr, n, k);
-    print(max_sum);
+    max_sum_naive = Solution.max_sum_of_subarray(arr, n, k)
+    max_sum_sw_1 = Solution.max_sum_of_Subarray_sliding_window_1(arr, n, k)
+    print(max_sum_naive)
+    print(max_sum_sw_1)
